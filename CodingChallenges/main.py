130c760985f3d0caeba36c41da9f556e2a870826
Sirfan123 <123samih101@gmail.com>
2023-12-05 16:48:09 -0500

    FRONTEND FINAL

--- CodingChallenges/main.py
+++ CodingChallenges/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, url_for, redirect, render_template, request, abort
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def default():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
